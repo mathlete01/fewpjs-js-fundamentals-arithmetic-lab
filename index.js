5abd9689bba63b90c97a650b4b9f9691be7f9469
mathlete01 <msallin@gmail.com>
2020-11-25 13:53:24 -0800

    commit tree b86c05f40f06e3f4f5c1a788ec369368652edee6

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+let num1 = 31
+let num2 = 2
+//let multiply = (num1, num2) => {return num1 * num2};
+const multiply = (num1 * num2)
 
-// Write your code here
+const random = Math.random() + 1
+
+const mod = 20/5
